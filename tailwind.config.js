38be1d6db3b9cb85e798edd51733554e5ff14f26
Guilherme Pereira de Oliveira <127159438+GuilhermePereiraCode@users.noreply.github.com>
2023-04-30 14:43:34 -0300

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'green-spotify': '#1ed760',
+        'pink-spotify': '#a1248b'
+      },
+    },
+  },
+  plugins: [],
+}
+
